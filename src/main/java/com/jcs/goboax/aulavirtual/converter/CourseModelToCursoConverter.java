d97fa2d59e7a2a38ea1e99675693d6a4403b307f
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-08-24 15:12:40 -0500

    Fixed Add course, NullPointerException

--- src/main/java/com/jcs/goboax/aulavirtual/converter/CourseModelToCursoConverter.java
+++ src/main/java/com/jcs/goboax/aulavirtual/converter/CourseModelToCursoConverter.java
@@ -3,6 +3,8 @@ package com.jcs.goboax.aulavirtual.converter;
 import com.jcs.goboax.aulavirtual.model.Curso;
 import com.jcs.goboax.aulavirtual.util.Constants;
 import com.jcs.goboax.aulavirtual.viewmodel.CourseModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 
 import java.util.Date;
@@ -10,11 +12,15 @@ import java.util.Date;
 public class CourseModelToCursoConverter
         implements Converter<CourseModel, Curso>
 {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseModelToCursoConverter.class);
     @Override
     public Curso convert(CourseModel courseModel)
     {
         Curso myCurso = new Curso();
-        myCurso.setCursoId(courseModel.getId());
+        if (courseModel.getId() != null)
+        {
+            myCurso.setCursoId(courseModel.getId());
+        }
         myCurso.setCreadoPor(Constants.SUPER_USER_ID);
         myCurso.setFechaCreacion(new Date());
         myCurso.setAudiencia(courseModel.getAddressedTo());
