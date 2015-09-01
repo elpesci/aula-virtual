a3c7be4e52ff5b37a6f7ba682e35e274c564f3e2
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2015-09-01 03:11:47 -0500

    Updated foreing keys and html mail format links

--- src/sql/003_avPhase2_add_Valoracion.sql
+++ src/sql/003_avPhase2_add_Valoracion.sql
@@ -6,5 +6,8 @@ CREATE TABLE `Valoracion` (
   `examen` blob,
   `creadoPor` int(11) DEFAULT NULL,
   `fechaCreacion` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`examenId`,`usuarioId`)
+  PRIMARY KEY (`examenId`,`usuarioId`),
+  KEY `Valoracion_fk2` (`usuarioId`),
+  CONSTRAINT `Valoracion_fk2` FOREIGN KEY (`usuarioId`) REFERENCES `Usuario` (`usuarioId`),
+  CONSTRAINT `Valoracion_fk1` FOREIGN KEY (`examenId`) REFERENCES `Examen` (`examenId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
